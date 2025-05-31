7e9a1257ad817b9e4035c874689f173f81da61b3
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2025-06-01 00:42:56 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -139,7 +139,7 @@ def handleIndex(index, dic):
     return index
 
 def get_progress_bar_string(pct):
-    pct = float(str(pct).strip('%'))
+pct = float(str(pct).strip('%'))
 p = min(max(pct, 0), 100)
 cFull = int(p // 10)  # 10 steps total
 p_str = '◉' * cFull + '◯' * (10 - cFull)
