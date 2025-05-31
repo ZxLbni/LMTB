bbdb547253c947e604003494085db466dbb62ee7
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2025-06-01 00:37:18 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -140,14 +140,10 @@ def handleIndex(index, dic):
 
 def get_progress_bar_string(pct):
     pct = float(str(pct).strip('%'))
-    p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
-    if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
+p = min(max(pct, 0), 100)
+cFull = int(p // 10)  # 10 steps total
+p_str = '◉' * cFull + '◯' * (10 - cFull)
+return f"[{p_str}]"
 
 
 def get_all_versions():
