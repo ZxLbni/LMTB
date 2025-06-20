3c18483e7861a30ea5157bce21272bf1b84db025
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2025-06-20 21:16:47 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -44,19 +44,19 @@ PAGE_NO      = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING   = "Upload"
-    STATUS_DOWNLOADING = "Download"
-    STATUS_CLONING     = "Clone"
-    STATUS_QUEUEDL     = "QueueDL"
-    STATUS_QUEUEUP     = "QueueUp"
-    STATUS_PAUSED      = "Pause"
-    STATUS_ARCHIVING   = "Archive"
-    STATUS_EXTRACTING  = "Extract"
-    STATUS_SPLITTING   = "Split"
-    STATUS_CHECKING    = "CheckUp"
-    STATUS_SEEDING     = "Seed"
-    STATUS_METADATA    = "Metadata"
-    STATUS_ATTACHMENT  = "Attachment"
+    STATUS_UPLOADING   = "📤 Upload"
+    STATUS_DOWNLOADING = "📥 Download"
+    STATUS_CLONING     = "🌀 Clone"
+    STATUS_QUEUEDL     = "⏳ QueueDL"
+    STATUS_QUEUEUP     = "📶 QueueUp"
+    STATUS_PAUSED      = "⏸️ Pause"
+    STATUS_ARCHIVING   = "🗃️ Archive"
+    STATUS_EXTRACTING  = "📂 Extract"
+    STATUS_SPLITTING   = "✂️ Split"
+    STATUS_CHECKING    = "🧐 CheckUp"
+    STATUS_SEEDING     = "🌱 Seed"
+    STATUS_METADATA    = "📝 Metadata"
+    STATUS_ATTACHMENT  = "📎 Attachment"
 
 
 class setInterval:
